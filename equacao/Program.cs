2477cdf06b05a4557627a07cb0b9694d944b0f66
heb-k <hebe0465@gmail.com>
2023-05-19 19:01:12 -0300

    first commit

---
+++ equacao/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                equacao e = new equacao();
+
+                Console.WriteLine("insira o valor a:");
+                e.a = double.Parse(Console.ReadLine());
+
+                while (e.a == 0)
+                {
+                    Console.WriteLine("A não pode ser igual a 0!!\nInsira um valor valido:");
+                    e.a = double.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("insira o valor b:");
+                e.b = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("insira o valor c:");
+                e.c = double.Parse(Console.ReadLine());
+
+                if (e.delta() < 0)
+                {
+                    Console.WriteLine("as raízes da equação são: " + e.x1() + "i e " + e.x2() + "i");
+                }
+                else
+                {
+                    Console.WriteLine("as raízes da equação são: " + e.x1() + " e " + e.x2());
+                }
+
+                Console.WriteLine("deseja recomeçar? s/n");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                char tecla = keyInfo.KeyChar;
+
+                while (true)
+                {
+                    if (char.ToUpper(tecla) == 'S')
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                    else if (char.ToUpper(tecla) == 'N')
+                    {
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        keyInfo = Console.ReadKey();
+                        tecla = keyInfo.KeyChar;
+                    }
+                }
+
+            }
+        }
+
+    }
+}
+
+
---
+++ equacao/equacao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equacao
+{
+    class equacao
+    {
+        public double a = 0, b = 0, c = 0;
+
+        public double delta()
+        {
+            return Math.Pow(b, 2) - 4 * a * c;
+        }
+
+        public double x1()
+        {
+            return (-b + Math.Sqrt(Math.Abs((delta())))) / (2 * a);
+        }
+
+        public double x2()
+        {
+            return (-b - Math.Sqrt(Math.Abs((delta())))) / (2 * a);
+        }
+
+    }
+}
